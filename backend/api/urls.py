89592bab2fa141cbe49d7fab9125f052500f635e
Surajit Sadhukhan <surajitsadhukhan870@gmail.com>
2026-06-07 14:38:23 +0530

    Add Firestore events API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import test_api
+from .views import test_api, get_events
 
 urlpatterns = [
     path('test/', test_api),
+    path('events/', get_events),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,26 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from .firebase_config import db
+
+
 @api_view(['GET'])
 def test_api(request):
     return Response({
         "message": "Backend Connected Successfully"
     })
+
+
+@api_view(['GET'])
+def get_events(request):
+    events_ref = db.collection('events')
+    docs = events_ref.stream()
+
+    events = []
+
+    for doc in docs:
+        event = doc.to_dict()
+        event['id'] = doc.id
+        events.append(event)
+
+    return Response(events)
